d324be0925d54f7f002fd68591aab9d641e7e3b0
Dinglydell <>
2022-04-10 15:57:20 +0100

    Bumped version number

--- src/main/java/dinglydell/tfice/TFIce.java
+++ src/main/java/dinglydell/tfice/TFIce.java
@@ -14,7 +14,7 @@ import dinglydell.tfice.block.BlockUnstableIce;
 public class TFIce
 {
     public static final String MODID = "tfice";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 	public static Block unstableIce;
     
     @EventHandler
